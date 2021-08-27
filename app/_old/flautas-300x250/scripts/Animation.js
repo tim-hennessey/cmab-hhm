d8b04fc9f8341e7c6f524b727decaaccd285f88c
tim.hennessey <Tim.Hennessey@deutschinc.com>
2021-08-26 17:35:28 -0700

    initial commit

---
+++ app/_old/flautas-300x250/scripts/Animation.js
@@ -0,0 +1,54 @@
+var app = app || {};
+
+
+app.Animation = (function () {
+
+	var banner = document.getElementById('banner');
+	var t = TweenMax;
+	var tl = new TimelineMax();
+
+	var txt = document.getElementById('txt');
+	var cta = document.getElementById('cta');
+	var img = document.getElementById('img');
+	var cover = document.getElementById('cover');
+
+	// --------------------------------------------------------------------------------------
+	// set default properties
+	function initialize() {
+		// DO NOT EDIT: reveals banner once loaded
+		t.set(banner, {opacity:1});
+		t.set(cta, {transformOrigin:"55px 130px", opacity:0, scale:.5});
+		t.set(txt, {x:-300, opacity:0});
+		t.set(img, {y:-250});
+	}
+
+	// --------------------------------------------------------------------------------------
+	// Starts the animation
+	function start() {
+
+		tl.to(cover, .75, {y:"+=250", ease:Circ.easeIn})
+
+		.to(txt, 1.5, {x:"+=300", ease:Elastic.easeOut.config(.50,.95)}, "-=.5")
+		.to(txt, .5, {autoAlpha:1}, "-=1.35")
+
+		.to(img, .5, {y:"+=250", ease:Circ.easeOut}, "-=1")
+
+		.to(cta, 1, {autoAlpha:1, scale:1, ease:Elastic.easeOut.config(1.15,.75)}, "-=.25");
+		
+	}
+
+	// --------------------------------------------------------------------------------------
+	// Stops the animation
+	function stop() {
+		console.log("stopping animation");
+	}
+
+	// --------------------------------------------------------------------------------------
+	// Publicly accessible methods and properties
+	return {
+		initialize:initialize,
+		start:start,
+		stop:stop
+	}
+
+})();
---
+++ app/soup-300x600/scripts/Animation.js
@@ -0,0 +1,54 @@
+var app = app || {};
+
+
+app.Animation = (function () {
+
+	var banner = document.getElementById('banner');
+	var t = TweenMax;
+	var tl = new TimelineMax();
+
+	var txt = document.getElementById('txt');
+	var cta = document.getElementById('cta');
+	var img1 = document.getElementById('img1');
+	var img2 = document.getElementById('img2');
+
+	// --------------------------------------------------------------------------------------
+	// set default properties
+	function initialize() {
+		// DO NOT EDIT: reveals banner once loaded
+		t.set(banner, {opacity:1});
+		t.set(cta, {transformOrigin:"77px 189px", opacity:0, scale:.5});
+		t.set(txt, {x:-300, opacity:0});
+		t.set(img1, {x:-300});
+		t.set(img2, {x:+300});
+	}
+
+	// --------------------------------------------------------------------------------------
+	// Starts the animation
+	function start() {
+
+		tl.to(img1, .5, {x:0, ease:Circ.easeOut}, "+=.15")
+		.to(img2, .5, {x:0, ease:Circ.easeOut}, "-=.4")
+
+		.to(txt, 1.5, {x:0, ease:Elastic.easeOut.config(.50,.95)}, "-=.5")
+		.to(txt, .5, {autoAlpha:1}, "-=1.35")
+
+		.to(cta, 1, {autoAlpha:1, scale:1, ease:Elastic.easeOut.config(1.15,.75)}, "-=.25");
+		
+	}
+
+	// --------------------------------------------------------------------------------------
+	// Stops the animation
+	function stop() {
+		console.log("stopping animation");
+	}
+
+	// --------------------------------------------------------------------------------------
+	// Publicly accessible methods and properties
+	return {
+		initialize:initialize,
+		start:start,
+		stop:stop
+	}
+
+})();
